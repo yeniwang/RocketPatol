9b93f200e5f2e657db4f4297909017732f93bc5f
Yeni Wang <ywang887@ucsc.edu>
2021-04-19 14:27:00 -0700

    Complete Half Project

--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
 let config = {
-    type: Phaser.CANVAS,
+    type: Phaser.AUTO,
     width: 640,
-    height: 480
+    height: 480,
+    scene: [ Menu, Play ]
 }
 let game = new Phaser.Game(config);
+// reserve keyboard vars
+let keyF, keyR, keyLEFT, keyRIGHT;
+
+// set UI sizes
+let borderUISize = game.config.height / 15;
+let borderPadding = borderUISize / 3;

7a21a80bcb2dc1bdecdf0551eba1591c3b442339
Yeni Wang <ywang887@ucsc.edu>
2021-04-23 15:19:29 -0700

    Fix spellings

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -8,11 +8,11 @@ class Play extends Phaser.Scene {
         this.load.image('rocket', './assets/rocket.png');
         this.load.image('spaceship', './assets/spaceship.png');
         this.load.image('starfield', './assets/starfield.png');
-        this.load.image('faststar', 'assets/faststar.png');
+        this.load.image('faststar', 'assets/fastStar.png');
         // load spritesheet
         this.load.spritesheet('explosion', 'assets/explosion.png', {frameWidth: 64, frameHeight: 32, startFrame: 0, endFrame: 9});
         // load background music
-        this.load.audio('bgm', 'assets/bgm.mp3');
+        this.load.audio('bgm', 'assets/BGM.mp3');
     }
 
     create() {
